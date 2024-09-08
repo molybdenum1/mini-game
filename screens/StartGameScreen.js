4848a7692d27095b6429bd3d3e592f9509adf8ab
Denis <kozelden4ik@gmail.com>
2024-09-08 14:28:47 +0300

    main screens

---
+++ screens/StartGameScreen.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { View, TextInput, Pressable } from "react-native";
+
+export default function StartGameScreen() {
+  return (
+    <View>
+      <TextInput />
+      {/* Buttons */}
+    </View>
+  );
+}
